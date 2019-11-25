b166b806014e7676c64322221ab38cc550fa7d06
Ashutosh Chauhan <masterashu@live.in>
2019-11-25 17:58:16 +0530

    adding test5 & 6 data models

--- lib/data_models.dart
+++ lib/data_models.dart
@@ -6,7 +6,7 @@ class LessonData {
   List<Test2Data> test2 = [];
   List<Test3Data> test3 = [];
   List<Test4Data> test4 = [];
-  List<Test6Data> test6 = [];  
+  List<Test6Data> test6 = [];
 
   LessonData(
       {this.title,
@@ -120,29 +120,29 @@ class Test2Data {
   }
 }
 
-class NumberList{
+class NumberList {
   List<int> numbers;
 
   NumberList({this.numbers});
 
-  factory NumberList.fromJSON(Map<String,dynamic> jsonData){
+  factory NumberList.fromJSON(Map<String, dynamic> jsonData) {
     List<int> nums = [];
-    for (var x in jsonData['numbers']){
+    for (var x in jsonData['numbers']) {
       nums.add(x);
     }
     return NumberList(numbers: nums);
   }
 }
 
-class Test3Data{
+class Test3Data {
   String name;
   List<NumberList> numberLists;
 
   Test3Data({this.name, this.numberLists});
 
-  factory Test3Data.fromJSON(Map<String, dynamic> jsonData){
-    List<NumberList> numbersList =[];
-    for (var x in jsonData['number_lists']){
+  factory Test3Data.fromJSON(Map<String, dynamic> jsonData) {
+    List<NumberList> numbersList = [];
+    for (var x in jsonData['number_lists']) {
       numbersList.add(NumberList.fromJSON(x));
     }
     return Test3Data(name: jsonData['name'], numberLists: numbersList);
@@ -176,34 +176,74 @@ class Test4Data {
   }
 }
 
-
-
-class PicturePairChoice {
+class ImageChoiceData {
+  String text;
   String picture;
-  String description;
   bool correct;
 
-  PicturePairChoice({this.picture, this.description, this.correct});
+  ImageChoiceData({this.text, this.picture, this.correct});
 
-  factory PicturePairChoice.fromJSON(Map<String, dynamic> jsonData) {
-    return PicturePairChoice(
-        picture: jsonData['picture'], description: jsonData['description'], correct: jsonData['correct']);
+  factory ImageChoiceData.fromJson(Map<String, dynamic> jsonData) {
+    return ImageChoiceData(
+        text: jsonData['text'],
+        picture: jsonData['image'],
+        correct: jsonData['correct']);
   }
 }
 
+class ImageQuestionData {
+  String text;
+  List<ImageChoiceData> options;
+
+  ImageQuestionData({this.text, this.options});
+
+  factory ImageQuestionData.fromJSON(Map<String, dynamic> jsonData) {
+    List<ImageChoiceData> opts = [];
+    for (var x in jsonData['options']) {
+      opts.add(ImageChoiceData.fromJson(x));
+    }
+    return ImageQuestionData(text: jsonData['text'], options: opts);
+  }
+}
+
+class Test5Data {
+  String name;
+  List<ImageQuestionData> questions;
+
+  Test5Data({this.name, this.questions});
+
+  factory Test5Data.fromJSON(Map<String, dynamic> jsonData) {
+    List<ImageQuestionData> ques = [];
+    for (var q in jsonData['questions']) {
+      ques.add(ImageQuestionData.fromJSON(q));
+    }
+    return Test5Data(name: jsonData['name'], questions: ques);
+  }
+}
+
+class PictureTextInput {
+  String picture;
+  String correctText;
+
+  PictureTextInput({this.picture, this.correctText});
+
+  factory PictureTextInput.fromJSON(Map<String, dynamic> jsonData) {
+    return PictureTextInput(
+        picture: jsonData['picture'], correctText: jsonData['correct_text']);
+  }
+}
 
 class Test6Data {
   String name;
-  List<PicturePairChoice> choices;
+  List<PictureTextInput> choices;
 
   Test6Data({this.name, this.choices});
 
   factory Test6Data.fromJSON(Map<String, dynamic> jsonData) {
-    List<PicturePairChoice> pics = [];
-    for (var x in jsonData['choices']){
-      pics.add(PicturePairChoice.fromJSON(x));
+    List<PictureTextInput> pics = [];
+    for (var x in jsonData['pictures']) {
+      pics.add(PictureTextInput.fromJSON(x));
     }
     return Test6Data(name: jsonData['name'], choices: pics);
   }
 }
-
