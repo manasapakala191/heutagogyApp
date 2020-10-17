4dfb5520673be3da420d5c5ab6db602f46cbdfe9
manasapakala191 <venkatasrimanasa@iiits.in>
2020-10-17 19:46:22 +0530

    0th

--- lib/models/userModel.dart
+++ lib/models/userModel.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+
+class UserModel extends ChangeNotifier{
+  String uid;
+  String name;
+  String roll;
+  String photoURL;
+  List<String> courses_enrolled;
+//  progress variable
+
+  UserModel({this.uid,this.name,this.roll,this.photoURL,this.courses_enrolled});
+  factory UserModel.fromJSON(){
+    return UserModel();
+  }
+}
