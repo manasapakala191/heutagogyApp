0dfa239c8c0abbfd1a42c2ed24aad362f8d3f994
Manasa PVS <venkatasrimanasa.p18@iiits.in>
2020-11-16 13:08:46 +0530

    minor fix

--- lib/screens/test_screens/multiple_choice_image_question_screen.dart
+++ lib/screens/test_screens/multiple_choice_image_question_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:heutagogy/hex_color.dart';
 import 'package:heutagogy/models/progress.dart';
-import 'package:heutagogy/models/studentProgress.dart';
 import 'package:heutagogy/models/test_type_models/multiple_choice_question_test.dart';
 import 'package:heutagogy/models/test_type_models/option_class.dart';
 import 'package:heutagogy/models/test_type_models/question_class.dart';
@@ -47,7 +46,7 @@ class _MultipleChoiceImageQuestionScreenState
   }
 
   void _updateProgress() {
-    var progress = Provider.of<StudentProgress>(context, listen: false);
+    // var progress = Provider.of<StudentProgress>(context, listen: false);
     List<String> responses = List<String>();
     var user = Provider.of<UserModel>(context, listen: false);
     String studentID = user.getID();
@@ -55,7 +54,6 @@ class _MultipleChoiceImageQuestionScreenState
       responses.add(_);
     }
     print(responses);
-    progress.addResponses(widget.courseID,widget.lessonID,widget.type,responses);
     int count = 0, total = 0;
     for (var _ in answers.values) {
       if (_) {
